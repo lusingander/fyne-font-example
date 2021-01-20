aea768b45212095dd419eab02288b9f4276a2bd9
Kyosuke Fujimoto <kyoro.f@gmail.com>
2021-01-20 18:02:50 +0900

    Add v2 sample

---
+++ v2/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+	a := app.New()
+	a.Settings().SetTheme(&myTheme{})
+	w := a.NewWindow("font")
+	w.Resize(fyne.NewSize(300, 200))
+	w.SetContent(
+		fyne.NewContainerWithLayout(
+			layout.NewVBoxLayout(),
+			layout.NewSpacer(),
+			widget.NewLabel("こんにちは、ファイン"),
+			widget.NewLabel("これは日本語のラベルです"),
+			widget.NewButton("これはボタンです", func() {
+				dialog.ShowInformation("確認", "これはダイアログです", w)
+			}),
+			layout.NewSpacer(),
+		),
+	)
+	w.ShowAndRun()
+}
